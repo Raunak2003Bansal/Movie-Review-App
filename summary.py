6772821f0fc9c5c2261e87e7c7ff89c27f05239e
Raunak2003Bansal <raunakbansal2003@gmail.com>
2025-11-17 12:13:10 +0530

    Fixed LLM integration and Streamlit secrets usage

--- summary.py
+++ summary.py
@@ -1,13 +1,10 @@
-from dotenv import load_dotenv
-import os
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_classic.chains import LLMChain
 from langchain_groq import ChatGroq
+import streamlit as st
 
-load_dotenv()
-
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
 llm = ChatGroq(api_key=GROQ_API_KEY,model = "llama-3.3-70b-versatile")
 
 def review_summary(reviews):
